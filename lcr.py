eb7807af4cec09d70a77c912d6a87cefd7beaa58
Sebastian- <hmurgu@interchange.ubc.ca>
2018-02-07 21:39:46 -0800

    Added exception classes to handle invalid working directory configuration

--- lcr.py
+++ lcr.py
@@ -1,5 +1,5 @@
-#! python3
-# lcr.py - a script for the mailing and attachment of kumon lcr reports
+#!/usr/bin/env python3
+"""A script to help automate the task of sending out LCR reports to parents."""
 
 import os
 import xlrd
@@ -15,50 +15,79 @@ from email.utils import COMMASPACE, formatdate
 from email import encoders
 
 
+
+class Error(Exception):
+    """Base error class for this module"""
+    pass
+
+
+class MissingFileError(Error):
+    """Raised when a directory is accessed but does not contain the necessary
+    files/data
+
+    Attributes:
+        file -- the name of the missing file
+        path -- the filepath of the directory being accessed
+    """
+
+    def __init__(self, file, path):
+        self.file = file
+        self.path = path
+
+
+class MissingColumnError(Error):
+    """Raised when a spreadsheet is missing a column required by the program
+
+    Attributes:
+        column -- the name of the required column
+        sheetName -- the name of the spreadsheet with the missing column
+    """
+
+    def __init__(self, col, sheet):
+        self.column = col
+        self.sheetName = sheet
+
+
 def lcr():
     baseDirectory = os.getcwd()
     templateEmailPath = baseDirectory + "\\TemplateEmail.html"
-    testTotalsPath = baseDirectory + "\\AchievementTestData.xls"
+    test_total_ss = baseDirectory + "\\AchievementTestData.xls"
 
-    targetDirectory = input("Enter the filepath containing lcr spreadsheets: ")
-    mathXLS = targetDirectory + "\\math.xls"
-    readingXLS = targetDirectory + "\\reading.xls"
-    while True:
-        try:
-            os.chdir(targetDirectory)
-        except OSError:
-            print("Invalid directory. Please try again.")
-            targetDirectory = input(
-                "Enter the filepath containing lcr spreadsheets: ")
-            continue
-        break
+    working_directory = getLCRDirectory()
+    math_ss = working_directory + "\\math.xls"
+    reading_ss = working_directory + "\\reading.xls"
 
-    checkTarget(targetDirectory)
-    checkSpreadsheets(targetDirectory)
-
-    """key is subject and test level (eg. 'Math 4A'),
-    value is a dict {'totalMarks': , 'suggestedTime': }"""
     testTotals = {}
-    """ key is subject and row number, value is a dict with all keys in
-    REQUIRED_COLUMNS and their respective values"""
+    """Contains all achievement test data pulled from test_total_ss
+        -- Key is a string with the subject and test level (eg. "Math 4A")
+        -- Value is a dictionary: {'totalMarks': , 'suggestedTime': }
+    """
+    
     lcrData = {}
+    """Contains all LCR testing data from math_ss and reading_ss
+        -- Key is a string with the subject and row number (eg. "Math 3")
+           corresponding single test text entry in the relevant spreadsheet
+        -- Value is a dictionary relating all REQUIRED_COLUMNS to their
+           relevant values
+    """
 
-    loadWorksheet(testTotalsPath, 0, ['level'], testTotals)
-    loadWorksheet(testTotalsPath, 1, ['level'], testTotals)
-    loadWorksheet(mathXLS, 0, ['Subject'], lcrData, rowNumbers=True)
-    loadWorksheet(readingXLS, 0, ['Subject'], lcrData, rowNumbers=True)
+    # Grab all the data contained in spreadsheets
+    loadWorksheet(test_total_ss, 0, ['level'], testTotals)
+    loadWorksheet(test_total_ss, 1, ['level'], testTotals)
+    loadWorksheet(math_ss, 0, ['Subject'], lcrData, rowNumbers=True)
+    loadWorksheet(reading_ss, 0, ['Subject'], lcrData, rowNumbers=True)
 
+    # Assign the relevant total score and suggested time for every test taken 
     assignTestTotals(testTotals, lcrData)
 
     """create a folder for each test taken to contain their respective email
     and pdf attachment"""
-    prepEmailFolders(lcrData, templateEmailPath, targetDirectory)
+    prepEmailFolders(lcrData, templateEmailPath, working_directory)
 
     """pdfs are imported by hand from CMS, then a check is done to ensure
     each folder has exactly one pdf"""
-    input("Please import all lcr PDFs into their respective folders. \
-        Press Enter when ready")
-    checkPDFs(targetDirectory)
+    input("Please import all LCR PDFs into their respective folders. Press Enter when ready...")
+    checkPDFs(working_directory)
 
     """prepare print folder"""
     try:
@@ -69,8 +98,8 @@ def lcr():
     user, smtp = emailLogin()
 
     # walk each student's folder and attempt to send an email
-    for folderName, subfolders, filenames in os.walk(targetDirectory):
-        if folderName == targetDirectory or folderName == targetDirectory + "\\To Print":
+    for folderName, subfolders, filenames in os.walk(working_directory):
+        if folderName == working_directory or folderName == working_directory + "\\To Print":
             continue
         studentInfo = lcrData[folderName.split(' --- ')[1]]
 
@@ -88,7 +117,7 @@ def lcr():
             toPrint = True
 
         if toPrint:
-            dest = targetDirectory + "\\To Print"
+            dest = working_directory + "\\To Print"
             src = attachmentPath
             print(studentInfo['FirstName'] + "'s LCR report must be printed. It has been moved to " + dest)
             shutil.move(src, dest)
@@ -101,15 +130,40 @@ def lcr():
     input("Press Enter to exit")
 
 
+def getLCRDirectory():
+    """TODO write this"""
+    current_dir = os.getcwd()
+    while True:
+        targetDirectory = input("Enter the filepath for the folder containing LCR spreadsheets: ")
+        try:
+            os.access(targetDirectory, os.F_OK)
+            checkTarget(targetDirectory)
+            checkLCRSpreadsheets(targetDirectory)
+        except OSError:
+            print("Invalid directory. Please try again.")
+            continue
+        except MissingFileError as err:
+            print("***Could not find " + err.file + " in "
+                  + err.path)
+            continue
+        except MissingColumnError as err:
+            print("***Missing column " + err.column + " in spreadsheet " 
+                  + err.sheetName)
+            continue
+        break
+    os.chdir(current_dir)
+    return targetDirectory
+
 # Ensure necessary columns are present in each spreadsheet
-def checkSpreadsheets(targetDirectory):
+def checkLCRSpreadsheets(targetDirectory):
+    current_dir = os.getcwd()
+    os.chdir(targetDirectory)
     REQUIRED_COLUMNS = ['FirstName', 'LastName', 'Subject', 'Type', 'Time',
                         'Score', 'FatherEmail', 'MotherEmail', 'Passing']
 
     print("Checking spreadsheets...")
     s = [x for x in os.listdir(".") if x.endswith(".xls")]
     for spreadsheet in s:
-        missingStuff = False
         workbook = xlrd.open_workbook("." + '\\' + spreadsheet)
         sheet = workbook.sheet_by_index(0)
         columns = []
@@ -117,11 +171,9 @@ def checkSpreadsheets(targetDirectory):
             columns.append(sheet.cell_value(0, col))
         for col in REQUIRED_COLUMNS:
             if col not in columns:
-                print("*** ERROR: Missing column '" + col + "' in " + spreadsheet)
-                missingStuff = True
-        if missingStuff:
-            input("Press Enter to exit.")
-            sys.exit()
+                os.chdir(current_dir)
+                raise MissingColumnError(col, spreadsheet)
+    os.chdir(current_dir)
 
 
 # Check that all lcr pdfs are reasonably sorted into folders
@@ -159,18 +211,17 @@ def checkPDFs(targetDirectory):
 
 # Ensure target folder contains correct excel files
 def checkTarget(targetDirectory):
+    # TODO raise MissingFileError
+    current_dir = os.getcwd()
+    os.chdir(targetDirectory)
     REQUIRED_SPREADSHEETS = ['math.xls', 'reading.xls']
     s = [x for x in os.listdir(".") if x.endswith(".xls")]
 
-    while(s == []):
-        print("No excel files found at " + targetDirectory)
-        targetDirectory = input("Enter the filepath containing lcr spreadsheets: ")
-        s = [x for x in os.listdir(".") if x.endswith(".xls")]
     for file in REQUIRED_SPREADSHEETS:
-        while file not in s:
-            input("Missing excel file " + file + " Ensure two excel files with names "
-                  + "'math' and 'reading' are present. Press Enter when ready.")
-            s = [x for x in os.listdir(".") if x.endswith(".xls")]
+        if file not in s:
+            os.chdir(current_dir)
+            raise MissingFileError(file, targetDirectory)
+    os.chdir(current_dir)
 
 
 # Loads the data of an excel worksheet into the dictionary 'data'
