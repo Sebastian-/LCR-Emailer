d5dfd75c961751344ca6ba6c9f2898b7a3c34a80
Sebastian- <hmurgu@interchange.ubc.ca>
2018-04-08 12:59:13 -0700

    Fixed bug which sent 'to print' attachments to the wrong destination

--- lcr.py
+++ lcr.py
@@ -122,7 +122,7 @@ def lcr():
         try:
             smtp.sendmail(user, msg['To'], msg.as_string())
         except smtplib.SMTPException:
-        	dest = print_folder + os.path.split(attachment)[1]
+        	dest = os.path.join(print_folder, os.path.basename(attachment))
             # If an email cannot be sent, the report must be printed
             print("Failed to send email to " + msg['To'])
             print("LCR report for " + student_info['FirstName'] + " "
